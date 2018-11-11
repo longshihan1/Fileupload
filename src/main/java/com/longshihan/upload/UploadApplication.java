47e8b77b2faa0c7b5493211b75c41582e651f736
longshihan <577093937@qq.com>
2018-11-11 23:30:43 +0800

    Initial commit

---
+++ src/main/java/com/longshihan/upload/UploadApplication.java
@@ -0,0 +1,40 @@
+package com.longshihan.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+
+@SpringBootApplication
+@RestController
+@Configuration
+public class UploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadApplication.class, args);
+    }
+
+    /**
+     * 文件上传配置
+     *
+     * @return
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //  单个数据大小
+        factory.setMaxFileSize("10240KB"); // KB,MB
+        /// 总上传数据大小
+        factory.setMaxRequestSize("102400KB");
+        return factory.createMultipartConfig();
+    }
+
+
+}
